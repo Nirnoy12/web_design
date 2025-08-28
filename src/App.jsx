976d2c057af788c05bef8e99f4a23408472f5cbe
Nirnoy Chatterjee <nirnoychatterjee2004@gmail.com>
2025-08-28 22:36:14 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import gsap from 'gsap';
+import {ScrollTrigger, SplitText} from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return (
+    <div className='flex-center h-[100vh]'>
+      <h1 className='text-3xl font-bold text-indigo-300'>
+        First time using react and tailwind properly
+      </h1>
+    </div>
+  );
+};
+
+export default App;
