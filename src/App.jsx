2b096aa64196a4cb7054de66d1b265e69af4cea7
Nirnoy Chatterjee <nirnoychatterjee2004@gmail.com>
2025-08-29 21:25:56 +0530

    navbar preparation done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import gsap from 'gsap';
 import {ScrollTrigger, SplitText} from 'gsap/all';
 
+import Navbar from './components/Navbar';
+
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className='flex-center h-[100vh]'>
-      <h1 className='text-3xl font-bold text-indigo-300'>
-        First time using react and tailwind properly
-      </h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 };
 
